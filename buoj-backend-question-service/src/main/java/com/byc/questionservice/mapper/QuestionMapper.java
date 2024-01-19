b1fc80580b0200c64a31f89fac1bd083d17de757
byc996 <buyicode@gmail.com>
2024-01-19 10:18:00 -0500

    update

--- buoj-backend-question-service/src/main/java/com/byc/questionservice/mapper/QuestionMapper.java
+++ buoj-backend-question-service/src/main/java/com/byc/questionservice/mapper/QuestionMapper.java
@@ -6,7 +6,6 @@ import com.byc.backendmodel.entity.Question;
 /**
 * @description 针对表【question(题目)】的数据库操作Mapper
 * @createDate 2023-08-07 20:58:00
-* @Entity com.byc.buoj.model.entity.Question
 */
 public interface QuestionMapper extends BaseMapper<Question> {
 
